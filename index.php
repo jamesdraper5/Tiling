484a858cb5077799d9e489f5823cffce43c20ca1
James Draper <jamesdrape@gmail.com>
2015-06-27 19:24:57 +0100

    Add new PhotoSwipe carousel

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		
+
 		<title>West Cork Tiling | Quality, Affordable Tiler in West Cork</title>
 		<meta name="google-site-verification" content="HR_ln3nc9XQTBDMcUra_ToNBNwYcYUOyyELsLfyXJlY" />
 		<meta name="keywords" content="tiling Cork, tiling West Cork, cheap tiling Cork, quality tiling Cork, budget tiling West Cork">
@@ -13,16 +13,17 @@
 		<meta itemprop="name" content="West Cork Tiling">
 		<meta itemprop="description" content="West Cork Tiling offers high quality tiling in the West Cork area at an affordable price. If you need a tiler in the West Cork area then look no further.">
 		<meta itemprop="url" content="http://www.westcorktiling.com" >
-		
+
 		<!-- <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Italianno|Mate+SC|Arvo" type="text/css"> -->
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,600,400|Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/reset.css" media="screen" />
 		<link rel="stylesheet" href="css/text.css" media="screen" />
-		
+
 		<!-- Bootstrap -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
-		
-		<link rel="stylesheet" href="bower_components/responsive-lightbox/jquery.lightbox.css" />
+
+		<link rel="stylesheet" href="bower_components/photo.swipe/dist/photoswipe.css" />
+		<link rel="stylesheet" href="bower_components/photo.swipe/dist/default-skin/default-skin.css" />
 
 		<style>
 			body {
@@ -34,7 +35,7 @@
 				font-weight: 300;
 			}
 			.uppercase {
-				text-transform: uppercase;	
+				text-transform: uppercase;
 			}
 			ul.disc {
 				list-style-type: circle;
@@ -63,7 +64,7 @@
 				background-color: transparent;
 			}
 			.hero_agency {
-				background-image: url(img/bathroom-tiles-dark.jpg); 
+				background-image: url(img/bathroom-tiles-dark.jpg);
 				text-align:center;
 				position: relative;
 				color: #fff;
@@ -89,7 +90,7 @@
 				color: #fff;
 				margin-bottom: 40px;
 			}
-			
+
 			.slideshow {
 				background: #333;
 				margin-top: 40px;
@@ -110,7 +111,7 @@
 			}
 		</style>
 
-		
+
 		<script type="text/javascript">
 
 		  var _gaq = _gaq || [];
@@ -125,7 +126,7 @@
 
 		</script>
 
-		
+
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -135,10 +136,10 @@
 		<![endif]-->
 	</head>
  	<body>
-		
 
 
-		
+
+
 		<!-- Static navbar -->
 		<nav class="navbar navbar-inverse navbar-fixed-top">
 			<div class="container">
@@ -160,7 +161,7 @@
 				</div><!--/.nav-collapse -->
 			</div>
 		</nav>
-		
+
 		<div class="hero hero_agency" id="home">
 			<div class="container">
 				<div class="row">
@@ -176,7 +177,7 @@
 						</p>
 
 						<!-- Hero buttons -->
-						<a href="#" class="hero__btn btn btn-lg2 btn-danger">Get a Quotation</a> 
+						<a href="#" class="hero__btn btn btn-lg2 btn-danger">Get a Quotation</a>
 						<a href="#about" class="hero__btn btn btn-lg2 btn-link">Find out more</a>
 
 					</div>
@@ -190,19 +191,19 @@
 				<div class="col-md-6 col-md-offset-3 text-center">
 					<h2 class="uppercase">About West Cork Tiling</h1>
 					<p>
-						West Cork Tiling offers a professional tiling service at an affordable price. 
+						West Cork Tiling offers a professional tiling service at an affordable price.
 						Our skilled tilers deliver a quality service to suit any budget and can also offer advice on the design and layout of your project.
 					</p>
 					<br>
-					<a id="workBtn" class="btn btn-danger" href="#" title="View our Work Gallery">View Our Work</a>
+					<a href="#" class="btn btn-danger js-show-gallery" title="View our Work Gallery">View Our Work</a>
 				</div>
 			</section>
 
-			
-			
+
+
 			<section class="row">
 				<section class="col-md-4">
-					<h2 class="uppercase">Our Tiling Services</h2>
+					<h2 class="uppercase">What We Do</h2>
 					<p>We perform a wide range of tiling jobs in the West Cork area including:</p>
 					<ul class="disc">
 						<li>Bathroom Tiling</li>
@@ -225,9 +226,9 @@
 					</ul>
 				</section>
 				<section class="col-md-4">
-					<h2 class="uppercase">What We Do</h2>
+					<h2 class="uppercase">Other Services</h2>
 					<p>
-						We can also prepare the area to be tiled, for example removing skirting boards before the area is tiled. 
+						We can also prepare the area to be tiled, for example removing skirting boards before the area is tiled.
 						No job is too big or too small, so why not take a look at some of our previous work and call us for a quote, or fill out our enquiries form and we will be in touch to discuss your requirements.
 					</p>
 					<br>
@@ -235,25 +236,25 @@
 				</section>
 			</section>
 
-			
+
 
 			<!---
 			<hr>
 
 			<div class="row">
-				
+
 				<div class="col-md-6 col-md-offset-3">
-				
+
 					<div id="photos-carousel" class="carousel slide" data-ride="carousel">
-					  	
-					  	
+
+
 					  	<ol class="carousel-indicators">
 						    <li data-target="#photos-carousel" data-slide-to="0" class="active"></li>
 						    <li data-target="#photos-carousel" data-slide-to="1"></li>
 						    <li data-target="#photos-carousel" data-slide-to="2"></li>
 					  	</ol>
 
-					  	
+
 					  	<div class="carousel-inner" role="listbox">
 
 						    <div class="item active">
@@ -268,7 +269,7 @@
 
 						</div>
 
-					
+
 						<a class="left carousel-control" href="#photos-carousel" role="button" data-slide="prev">
 							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 							<span class="sr-only">Previous</span>
@@ -282,7 +283,7 @@
 
 			</div>
 			<br/>
-			
+
 
 			<div class="row">
 				<section class="col-md-6 col-md-offset-3">
@@ -295,7 +296,201 @@
 			<hr>
 			-->
 		</div>
-		
+
+
+
+
+		<!-- Root element of PhotoSwipe. Must have class pswp. -->
+		<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+
+		    <!-- Background of PhotoSwipe.
+		         It's a separate element, as animating opacity is faster than rgba(). -->
+		    <div class="pswp__bg"></div>
+
+		    <!-- Slides wrapper with overflow:hidden. -->
+		    <div class="pswp__scroll-wrap">
+
+		        <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
+		        <div class="pswp__container">
+		            <!-- don't modify these 3 pswp__item elements, data is added later on -->
+		            <div class="pswp__item"></div>
+		            <div class="pswp__item"></div>
+		            <div class="pswp__item"></div>
+		        </div>
+
+		        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+		        <div class="pswp__ui pswp__ui--hidden">
+
+		            <div class="pswp__top-bar">
+
+		                <!--  Controls are self-explanatory. Order can be changed. -->
+
+		                <div class="pswp__counter"></div>
+
+		                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+
+		                <button class="pswp__button pswp__button--share" title="Share"></button>
+
+		                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+
+		                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+
+		                <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
+		                <!-- element will get class pswp__preloader--active when preloader is running -->
+		                <div class="pswp__preloader">
+		                    <div class="pswp__preloader__icn">
+		                      	<div class="pswp__preloader__cut">
+		                        	<div class="pswp__preloader__donut"></div>
+		                      	</div>
+		                    </div>
+		                </div>
+		            </div>
+
+		            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+		                <div class="pswp__share-tooltip"></div>
+		            </div>
+
+		            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>
+
+		            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>
+
+		            <div class="pswp__caption">
+		                <div class="pswp__caption__center"></div>
+		            </div>
+
+		          </div>
+
+		    </div>
+
+		</div>
+
+
+		<!--
+		<div itemscope itemtype="http://schema.org/ImageGallery">
+
+		    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-1.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 1</figcaption>
+		        </a>
+		    </figure>
+
+		    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-2.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 2</figcaption>
+		        </a>
+		    </figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-1.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 1</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-2.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 2</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-1.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 1</figcaption>
+		        </a>
+		    </figure>
+
+		    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-2.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 2</figcaption>
+		        </a>
+		    </figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-1.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 1</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-2.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 2</figcaption>
+			    </a>
+			</figure>
+
+		    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-1.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 1</figcaption>
+		        </a>
+		    </figure>
+
+		    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-2.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 2</figcaption>
+		        </a>
+		    </figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-1.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 1</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-2.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 2</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-1.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 1</figcaption>
+		        </a>
+		    </figure>
+
+		    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-2.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 2</figcaption>
+		        </a>
+		    </figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-1.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 1</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-2.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 2</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-1.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 1</figcaption>
+		        </a>
+		    </figure>
+
+		    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+		        <a href="large-image-2.jpg" itemprop="contentUrl">
+		            <figcaption itemprop="caption description">Long image description 2</figcaption>
+		        </a>
+		    </figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-1.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 1</figcaption>
+			    </a>
+			</figure>
+
+			<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+			    <a href="large-image-2.jpg" itemprop="contentUrl">
+			        <figcaption itemprop="caption description">Long image description 2</figcaption>
+			    </a>
+			</figure>
+		</div>
+
+
+
 
 		<div class="gallery">
 			<a href="img/tiling-work/bath.jpg" data-caption="Tiled bathroom in Skibbereen">s</a>
@@ -319,10 +514,11 @@
 			<a href="img/tiling-work/tiling-cork.jpg" data-caption="Decorative tiling in Dunmanway" >s</a>
 			<a href="img/tiling-work/cork-bathroom-tiling-1.jpg" data-caption="Bathroom tiling in Clonakilty" >s</a>
 		</div>
+		-->
 
 		<hr>
-		
-		
+
+
 
 		<div class="container">
 			<div class="row">
@@ -351,13 +547,13 @@
 			<small>&copy; <?php echo date("Y"); ?> Bob Draper Tiling</small>
 		</footer>
 
-		
+
 
 
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<!-- Include all compiled plugins (below), or include individual files as needed 
+		<!-- Include all compiled plugins (below), or include individual files as needed
 		<script src="js/bootstrap.min.js"></script>
 		-->
 
@@ -366,14 +562,142 @@
 		<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
-		<script type="text/javascript" src="bower_components/responsive-lightbox/jquery.lightbox.js"></script>
+		<script type="text/javascript" src="bower_components/photo.swipe/dist/photoswipe.min.js"></script>
+		<script type="text/javascript" src="bower_components/photo.swipe/dist/photoswipe-ui-default.min.js"></script>
+
 
 		<script src="js/script.js"></script>
 
 		<script>
 			$(document).ready(function(){
-				//console.log( 'ready', $('.gallery a').length);
-				$('.gallery a').lightbox();
+				var openPhotoSwipe = function(e) {
+					e.preventDefault();
+				    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+				    // build items array
+				    var items = [
+				        {
+				            src: "img/tiling-work/bath.jpg",
+				            w: 836,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/kitchen-tiling-4.jpg",
+				            w: 1000,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/bathroom-tiling-cork.jpg",
+				            w: 400,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/floor-tiling.jpg",
+				            w: 720,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/bathroom-tiling-cork-3.jpg",
+				            w: 430,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/bathroom-tiling-cork-4.jpg",
+				            w: 450,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/floor-tiling-2.jpg",
+				            w: 550,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/tiling-shower.jpg",
+				            w: 400,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/kitchen-tiling.jpg",
+				            w: 800,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/bathroom-tiling-cork-2.jpg",
+				            w: 800,
+				            h: 530
+				        },
+				        {
+				            src: "img/tiling-work/kitchen-tiling-2.jpg",
+				            w: 800,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/kitchen-tiling-3.jpg",
+				            w: 800,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/cork-bathroom-tiling-3.jpg",
+				            w: 450,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/kitchen-tiling-cork.jpg",
+				            w: 800,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/mosaic-tiling-cork.jpg",
+				            w: 750,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/cork-bathroom-tiling-2.jpg",
+				            w: 800,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/mosaic-tiling.jpg",
+				            w: 750,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/mosaic-tiling-2.jpg",
+				            w: 815,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/tiling-cork.jpg",
+				            w: 800,
+				            h: 600
+				        },
+				        {
+				            src: "img/tiling-work/cork-bathroom-tiling-1.jpg",
+				            w: 800,
+				            h: 600
+				        }
+				    ];
+
+
+
+				    // define options (if needed)
+				    var options = {
+				             // history & focus options are disabled on CodePen
+				        history: false,
+				        focus: false,
+
+				        showAnimationDuration: 0,
+				        hideAnimationDuration: 0
+
+				    };
+
+				    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+				    gallery.init();
+				};
+
+				//openPhotoSwipe();
+
+				$('.js-show-gallery').on('click', openPhotoSwipe);
 			});
 		</script>
   	</body>
