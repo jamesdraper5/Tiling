2d1c31f87ee3cce04ee65c8ef7e9a6fb1080d788
James Draper <jamesdrape@gmail.com>
2015-06-30 19:02:21 +0100

    Work page - WIP

---
+++ page-work.inc.php
@@ -0,0 +1,52 @@
+<h1>Affordable Tiling in West Cork</h1>
+<section class="workItem">
+	<div class="grid_6 workImg wall">
+		<p class="sliderCTA">Click to view more tiling samples</p>
+	</div>
+	<div class="grid_6">
+		<h1>Wall &amp; Floor Tiling</h1>
+		<p>We offer a wide range of general tiling services including walls and floors. Wall tiles can be made from a wide variety of materials such as ceramic, terracotta or even glass, while floor tiles are generally made from a more hard wearing or coarse material such as slate or natural stone. If you need advice on which materials are the most suitable you can contact us to discuss your requirements.</p>
+	</div>
+</section>
+
+<div class="clearfix"></div>
+
+<section class="workItem">
+	<div class="grid_6 workImg kitchen">
+		<p class="sliderCTA">Click to view more tiling samples</p>
+	</div>
+	<div class="grid_6">
+		<h1>Kitchen Tiling</h1>
+		<p>When tiling a kitchen area it is important to select a tile with a smooth surface which can be easily wiped clean. We can help you to select the most suitable tiles for this purpose and offer advice on attractive design patterns. We can carry out all common kitchen tiling jobs such as counters, worktops, kitchen islands, splashbacks, walls and floors.</p>
+	</div>
+</section>
+
+<div class="clearfix"></div>
+
+<section class="workItem">
+	<div class="grid_6 workImg bathroom">
+		<p class="sliderCTA">Click to view more tiling samples</p>
+	</div>
+	<div class="grid_6">
+		<h1>Bathroom Tiling</h1>
+		<p>The bathroom is the most commonly tiled room in the house and provides an opportunity to create a visually striking design with your tiles. Whether you require intricate mosaic patterns or a simplistic clean design, we can offer you a tailor made solution that will meet your needs. As well as tiling bathroom walls and floors, we can also tile shower areas, baths and wet areas.</p>
+	</div>
+</section>
+
+<div class="grid_12 border"></div>
+	<section class="grid_9 contact">
+		<ul>
+			<li class="phone"><span class="icon"><i></i></span><span itemprop="telephone">(028) 38460</span></li>
+			<li class="mobile"><span class="icon"><i></i></span><span itemprop="telephone">(087) 6244562</span></li>
+			<li class="email"><span class="icon"><i></i></span><span itemprop="email">info@westcorktiling.com</span></li>
+		</ul>
+	</section>
+
+	<section class="grid_3">
+		</ul>
+		<p id="cta-div">
+			<a id="quoteBtn" class="cta rht" href="contact.php" title="Get a quotation">Get a Quotation</a>
+		</p>
+	</section>
+
+</div> <!-- End container_12 -->
