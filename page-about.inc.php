5fff06c32a9a07faadf1d15094dfee3b95a586c9
James Draper <jamesdrape@gmail.com>
2016-01-09 16:12:16 +0000

    Add styles to About page

--- page-about.inc.php
+++ page-about.inc.php
@@ -1,5 +1,23 @@
-<section itemscope itemtype="http://schema.org/PostalAddress" >
-	<h1>About West Cork Tiling</h1>
-	<p>West Cork Tiling is a website promoting the services of Bob Draper, a professional tiler based near <span itemprop="addressLocality">Skibbereen</span> in County <span itemprop="addressRegion">Cork</span>. Bob has over ten years experience in the tiling industry and prides himself on his high quality work and attention to detail. Having worked in the construction industry for almost thirty years, Bob has a wealth of experience and expertise in tiling and general construction subjects, and is always willing to offer advice on your tiling project.</p>
-	<p>Tiling services are available anywhere in the <span itemprop="addressRegion">West Cork</span> area, from <span itemprop="addressLocality">Dunmanway</span> to <span itemprop="addressLocality">Clonakilty</span> and <span itemprop="addressLocality">Schull</span> to <span itemprop="addressLocality">Bandon</span>.</p>
-</section>
+<div class="main-section top-padding">
+	<div class="container">
+		<section itemscope itemtype="http://schema.org/PostalAddress" >
+
+		    <div class="row">
+		    	<div class="col-md-7">
+					<h1>About West Cork Tiling</h1>
+					<p>West Cork Tiling is a website promoting the services of Bob Draper, a professional tiler based near <span itemprop="addressLocality">Skibbereen</span> in County <span itemprop="addressRegion">Cork</span>. Bob has over ten years experience in the tiling industry and prides himself on his high quality work and attention to detail. Having worked in the construction industry for almost thirty years, Bob has a wealth of experience and expertise in tiling and general construction subjects, and is always willing to offer advice on your tiling project.</p>
+					<p>Tiling services are available anywhere in the <span itemprop="addressRegion">West Cork</span> area, from <span itemprop="addressLocality">Dunmanway</span> to <span itemprop="addressLocality">Clonakilty</span> and <span itemprop="addressLocality">Schull</span> to <span itemprop="addressLocality">Bandon</span>.</p>
+					<p>No job is too big or too small, so why not take a look at some of our previous <a href="tiling-work">tiling jobs</a> and call us for a quote, or fill out our <a href="contact">enquiries form</a> and we will be in touch to discuss your requirements.</p>
+		    	</div>
+				<div class="col-md-5">
+					<div class="margintop20">
+						<a href="tiling-work">
+							<img src="img/tiling-work/kitchen-tiling-4.jpg" class="img-thumbnail" alt="Tiled bathroom with slanted ceiling in Bantry, Co. Cork">
+						</a>
+					</div>
+				</div>
+		    </div>
+
+		</section>
+	</div>
+</div>
